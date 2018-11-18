6e0fa24c05b602a610b3f5477223bec07d9cc965
voxace <dave@voxace.com>
2018-11-18 15:30:12 +1100

    first

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title>Hawkos App</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn flat to="/">Home</v-btn>
+        <v-btn flat to="/info">Info</v-btn>
+        <v-btn flat to="/contact">Contact</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <nuxt />
+    </v-content>
+    <v-footer app fixed />
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <v-layout>
+    <v-flex xs12>
+      <v-carousel>
+        <v-carousel-item src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" />
+      </v-carousel>
+    </v-flex>
+  </v-layout>
+
+  <v-container grid-list-xl>
+  <v-layout>
+        <v-flex xs4>
+      <v-card color="grey" dark>
+        <v-card-title primary class="title">Heading</v-card-title>
+        <v-card-text>Text goes here</v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs4>
+      <v-card color="grey" dark>
+        <v-card-title primary class="title">Heading</v-card-title>
+        <v-card-text>Text goes here</v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs4>
+      <v-card color="grey" dark>
+        <v-card-title primary class="title">Heading</v-card-title>
+        <v-card-text>Text goes here</v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  </v-container>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+.border {border:1px solid black;}
